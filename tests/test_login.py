4b888a92d2befb6b033f8aa943c7525937c34b5b
Kirk Kaaua <kauakirk46@gmail.com>
2026-06-15 23:45:47 -0300

    test: assert error body fields on empty login fields test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,6 @@
 from services.usuarios_service import UsuariosService
 from utils.payloads import new_user_payload, login_payload, empty_login_payload
+from utils.validator import validate_user_login, validate_login_response
 import uuid
 
 
@@ -8,10 +9,14 @@ def test_login_with_valid_credentials():
     create_response = UsuariosService.create(user_payload)
     assert create_response.status_code == 201
 
-    login_response = UsuariosService.login(login_payload(user_payload["email"], user_payload["password"]))
+    login_payload_body = login_payload(user_payload["email"], user_payload["password"])
+    validate_user_login(login_payload_body)
+
+    login_response = UsuariosService.login(login_payload_body)
     body = login_response.json()
 
     assert login_response.status_code == 200
+    validate_login_response(body)
     assert body["message"] == "Login realizado com sucesso"
     assert "authorization" in body
 
@@ -38,4 +43,7 @@ def test_login_with_nonexistent_email():
 
 def test_login_with_empty_fields():
     login_response = UsuariosService.login(empty_login_payload())
+    body = login_response.json()
+
     assert login_response.status_code == 400
+    assert "email" in body or "password" in body
